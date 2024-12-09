74697908a8f9f74310fa6da0ea9116e4ff2748c9
strayosz <kamilyakupov01@gmail.com>
2024-12-09 23:09:46 +0300

    Create class UserService | Kamil Yakupov

---
+++ src/src/module/UserService.java
@@ -0,0 +1,10 @@
+public class UserService {
+    public User[] create4Users() {
+        User[] users = new User[4];
+        users[0] = new User("qwverh", "Artem", "77777777777", "123456789");
+        users[1] = new User("strayosz", "Kamil", "77777777777", "987654321");
+        users[2] = new User("kibersashka", "Aleksandra", "77777777777", "192837465");
+        users[3] = new User("asdfasf", "AKA", "77777777777", "918273645");
+        return users;
+    }
+}
