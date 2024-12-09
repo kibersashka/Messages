63b328f684777315f549e4e32f7bfde99810d09b
strayosz <kamilyakupov01@gmail.com>
2024-12-09 23:24:36 +0300

    Debugging method create4Users | Kamil Yakupov

--- src/java/test/java/UserService.java
+++ src/java/test/java/UserService.java
@@ -1,5 +1,5 @@
 public class UserService {
-    public User[] create4Users() {
+    public static User[] create4Users() {
         User[] users = new User[4];
         users[0] = new User("qwverh", "Artem", "77777777777", "123456789");
         users[1] = new User("strayosz", "Kamil", "77777777777", "987654321");
